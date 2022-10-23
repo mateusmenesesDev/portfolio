5674893f2c082fb8b1f101ed82d73c73c0f64f22
Mateus Meneses <mateusppa10@gmail.com>
2022-10-23 19:29:24 -0300

    :construction: Atualizando componente About e Experience

--- src/components/About/Experience/index.tsx
+++ src/components/About/Experience/index.tsx
@@ -1,35 +1,37 @@
 import style from './Experience.module.css'
-
+import {DiScrum} from 'react-icons/di'
 export function Experience(){
     return (
         <section className={style.experience}>
                 <div className={style['experience-years']}>
                     <div className={style['experience-years--border']}>
-                        <i className="fa-solid fa-code fa-4x"></i>
+                        <i className="fa-solid fa-code fa-3x"></i>
                         <p className={style['experience-years--secondary']}>1 ano como</p>
                         <p className={style['experience-years--main']}>Desenvolvedor</p>
                     </div>
-                    <div>
+                    <div className={style['experience-years--noborder']}>
+                        <i className="fa-solid fa-briefcase fa-3x main"></i>
                         <p className={style['experience-years--secondary']}>3 anos de</p>
                         <p className={style['experience-years--main']}>Trabalhos</p>
                     </div>
                     <div className={style['experience-years--border']}>
+                        <DiScrum size={92} color='#FFC300'/>
                         <p className={style['experience-years--secondary']}>2 anos de</p>
                         <p className={style['experience-years--main']}>SCRUM</p>
                     </div>
                 </div>
                 <div className={style['experience-jobs']}>
                     <div>
-                        <p>Gestor em</p>
-                        <p>T.I</p>
+                        <p className={style['experience-jobs--secondary']}>Gestor em</p>
+                        <p className={style['experience-jobs--main']}>T.I</p>
                     </div>
                     <div>
-                        <p>Gestor em</p>
-                        <p>T.I</p>
+                        <p className={style['experience-jobs--secondary']}>Participação como P.O em diversos</p>
+                        <p className={style['experience-jobs--main']}>Projetos</p>
                     </div>
                     <div>
-                        <p>Gestor em</p>
-                        <p>T.I</p>
+                        <p className={style['experience-jobs--secondary']}>Diversas ferramentas</p>
+                        <p className={style['experience-jobs--main']}>AGILE</p>
                     </div>
                 </div>
         </section>
