113c380bc3de93db6e5a830b8548d3d8022fbe5b
Mateus Meneses <mateusppa10@gmail.com>
2022-10-26 17:16:27 -0300

    Instalando netlify-cli

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,9 +1,13 @@
 import axios from "axios"
-import { Octokit } from "octokit"
 import { accessToken } from "../config/access"
 import { IProjetos } from "../types/types"
+import {VercelRequest, VercelResponse} from '@vercel/node'
 
-
+export default (req:VercelRequest, res:VercelResponse) =>{
+    return res.json({
+        msg: 'hello world'
+    })
+}
 const githubApi = axios.create({
     baseURL: 'https://api.github.com/',
     headers:{'Authorization': `Bearer ${accessToken}`}
