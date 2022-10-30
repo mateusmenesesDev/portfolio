a3cbd9519cfcfa7dc00e2b7cfce8f594eae8f831
Mateus Meneses <mateusppa10@gmail.com>
2022-10-29 22:16:47 -0300

    Excluindo pasta api e concretizando chamada da api via netlify functions

--- src/components/Portfolio/Projetos/index.tsx
+++ src/components/Portfolio/Projetos/index.tsx
@@ -1,24 +1,37 @@
 import { IProjetos } from "../../../types/types";
 import { Badge } from "../../Badge";
-import style from './Projetos.module.css'
+import style from "./Projetos.module.css";
 
-export function Projetos({name, description, topics, html_url, homepage}:IProjetos){
-    return (
-        <div className={style.projeto}>
-            <h3>{name.toUpperCase()}</h3>
-            <p>{description}</p>
-            <div className={style.badges}>
-                {topics.map(topic=>(
-                    <Badge size="14px">{topic}</Badge>
-                ))}
-            </div>
-            <div className={style['projeto-img']}>
-                <img src={`https://raw.githubusercontent.com/mateusmenesesDev/${name}/main/preview/preview.png`} alt="" />
-            </div>
-            <div className={style['projeto-button']}>
-                <a href={homepage} target="_blank">Acessar Projeto</a>
-                <a href={html_url} target="_blank">Acessar Repositório</a>
-            </div>
-        </div>
-    )
+export function Projetos({
+  name,
+  description,
+  topics,
+  html_url,
+  homepage,
+}: IProjetos) {
+  return (
+    <div className={style.projeto}>
+      <h3>{name.toUpperCase()}</h3>
+      <p>{description}</p>
+      <div className={style.badges}>
+        {topics.map((topic) => (
+          <Badge size="14px">{topic}</Badge>
+        ))}
+      </div>
+      <div className={style["projeto-img"]}>
+        <img
+          src={`https://raw.githubusercontent.com/mateusmenesesDev/${name}/main/preview/preview.png`}
+          alt=""
+        />
+      </div>
+      <div className={style["projeto-button"]}>
+        <a href={homepage} target="_blank">
+          Acessar Projeto
+        </a>
+        <a href={html_url} target="_blank">
+          Acessar Repositório
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,45 +1,49 @@
-import { useEffect, useState } from 'react'
-import { apiGitHub } from '../../api/github'
-import { IProjetos } from '../../types/types'
-import { Badge } from '../Badge'
-import style from './Portfolio.module.css'
-import { Projetos } from './Projetos'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IProjetos } from "../../types/types";
+import { Badge } from "../Badge";
+import style from "./Portfolio.module.css";
+import { Projetos } from "./Projetos";
 
+export function Portfolio() {
+  const [projetos, setProjetos] = useState<IProjetos[] | []>([]);
 
-export function Portfolio(){
-    const[projetos, setProjetos] = useState<IProjetos[]|[]>([])
-    async function loadProjects(){
-        const response:IProjetos[] = await apiGitHub.listRepo()
-        setProjetos([...response])
-        
-    }
-    useEffect(()=>{
-        loadProjects()
-    },[])
-    // console.log(projetos)
-    return (
-        <section className={style.portfolio}>
-            <Badge fill>Portfólio</Badge>
-            <div className={style['portfolio-header']}>
-                <h2>Trabalhos e projetos</h2>
-                <div className={style['portfolio-header--filters']}>
-                    <Badge fill pointer>Front End</Badge>
-                    <Badge pointer>Fullstack</Badge>
-                    <Badge pointer>Back End</Badge>
-                </div>
-            </div>
-            <div className={style['portfolio-cards']}>
-                {projetos.map(projeto=>(
-                    <Projetos 
-                        homepage={projeto.homepage} 
-                        id={projeto.id} name={projeto.name} 
-                        description={projeto.description} 
-                        url={projeto.url} 
-                        topics={projeto.topics}
-                        html_url={projeto.html_url}
-                    /> 
-                ))}
-            </div>
-        </section>
-    )
+  const fetchData = async () => {
+    const results = await axios.get("/.netlify/functions/gitHub");
+    const repo: IProjetos[] = results.data;
+    setProjetos([...repo]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section className={style.portfolio}>
+      <Badge fill>Portfólio</Badge>
+      <div className={style["portfolio-header"]}>
+        <h2>Trabalhos e projetos</h2>
+        <div className={style["portfolio-header--filters"]}>
+          <Badge fill pointer>
+            Front End
+          </Badge>
+          <Badge pointer>Fullstack</Badge>
+          <Badge pointer>Back End</Badge>
+        </div>
+      </div>
+      <div className={style["portfolio-cards"]}>
+        {projetos.map((projeto) => (
+          <Projetos
+            homepage={projeto.homepage}
+            id={projeto.id}
+            name={projeto.name}
+            description={projeto.description}
+            url={projeto.url}
+            topics={projeto.topics}
+            html_url={projeto.html_url}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
