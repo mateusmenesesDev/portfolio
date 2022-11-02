4832128b040b58c53720d5b18cffcaa07c9efdcc
Mateus Meneses <mateusppa10@gmail.com>
2022-11-02 11:09:22 -0300

    Finalização do componente de Carrer

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -27,11 +27,6 @@ export function Portfolio() {
       <div className={style["portfolio-header"]}>
         <h2>Trabalhos e projetos</h2>
         <div className={style["portfolio-header--filters"]}>
-          <Badge fill pointer>
-            Front End
-          </Badge>
-          <Badge pointer>Fullstack</Badge>
-          <Badge pointer>Back End</Badge>
         </div>
       </div>
       <div className={style["portfolio-cards"]}>
