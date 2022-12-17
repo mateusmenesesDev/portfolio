a476c4a4bfd6bab03f83f43799963f5e77e62bae
Mateus <mateusppa10@gmail.com>
2022-12-17 16:41:06 -0300

    Mudando apresentação dos projetos

--- src/components/Portfolio/Projetos/index.tsx
+++ src/components/Portfolio/Projetos/index.tsx
@@ -1,6 +1,6 @@
-import { IProjetos } from "../../../types/types";
-import { Badge } from "../../Badge";
-import style from "./Projetos.module.css";
+import { IProjetos } from '../../../types/types';
+import { Badge } from '../../Badge';
+import style from './Projetos.module.css';
 
 export function Projetos({
   name,
@@ -11,34 +11,35 @@ export function Projetos({
 }: IProjetos) {
   return (
     <div className={style.projeto}>
-      <div className={style["projeto-img"]}>
+      <div className={style['projeto-img']}>
         <img
-          src={`https://raw.githubusercontent.com/mateusmenesesDev/${name}/main/preview/preview.png`}
-          alt="Imagem do Projeto"
+          src={`https://raw.githubusercontent.com/mateusmenesesDev/${name}/main/preview/preview2.png`}
+          alt='Imagem do Projeto'
         />
       </div>
       <div className={style['projeto-buttons']}>
-        <a href={homepage} target='_blank'>Acessar Projeto</a>
-        <a href={html_url} target='_blank'>Acessar Repositório</a>
+        <a href={homepage} target='_blank'>
+          Acessar Projeto
+        </a>
+        <a href={html_url} target='_blank'>
+          Acessar Repositório
+        </a>
       </div>
       <div className={style['projeto-details']}>
         <div className={style['projeto-details--left']}>
-          <div className={style['projeto-details--title']}>
-            {name}
-          </div>
+          <div className={style['projeto-details--title']}>{name}</div>
           <div className={style['projeto-details--badges']}>
-            {topics.map(topic => (
-              <Badge fill size="16px">{topic}</Badge>
+            {topics.map((topic) => (
+              <Badge fill size='16px'>
+                {topic}
+              </Badge>
             ))}
           </div>
         </div>
         <div className={style['projeto-details--right']}>
-          <div>
-            {description}
-          </div>
+          <div>{description}</div>
         </div>
       </div>
-     
     </div>
   );
 }
